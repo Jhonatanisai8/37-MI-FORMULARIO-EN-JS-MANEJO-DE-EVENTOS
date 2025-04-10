af982871a4c6b3bbca71b4c9aabeb6b3a9b1c6ce
jhonatan <ojedasnachezjhonatan@gmail.com>
2025-04-10 10:56:43 -0500

    evento de onChange en inputs de tipo select, options

--- code.js
+++ code.js
@@ -5,7 +5,10 @@ let contrasenia = 'admin123'
 function verificar() {
     let clave = document.getElementById('password').value
     let user = document.getElementById('userName').value
-
+    let indexSeleccion = document.getElementById('lenguajes')
+    if (indexSeleccion.selectedIndex == 0) {
+        alert('Por favor seleccionar una opcion')
+    }
     if (clave != contrasenia && userName != user) {
         alert('Clave  y Usuario incorrectos, Intente de nuevo')
         control++
@@ -15,4 +18,12 @@ function verificar() {
     } else {
         window.open("htpps://www.google.com")
     }
+}
+
+
+function seleccionarLenguaje() {
+    let indexSeleccion = document.getElementById('lenguajes')
+    document.getElementById('text01').value = indexSeleccion.selectedIndex
+    document.getElementById('text02').value = indexSeleccion.options[indexSeleccion.selectedIndex].text
+    document.getElementById('text03').value = indexSeleccion.options[indexSeleccion.selectedIndex].value
 }
